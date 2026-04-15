29b3df355a07fa96354816637cd608d0db16ada0
Stephen1902 <s_nichols_other@yahoo.co.uk>
2026-04-15 18:56:30 +0100

    Replaced US_GameState with US_GameStateBase as both GameState and GameMode have to inherit from the base class to work correctly.
    Added code to Player Character for movement, looking, sprinting and interaction.

---
+++ Source/UnrealShadows_LOTL/US_GameStateBase.h
@@ -0,0 +1,17 @@
+// Copyright 2026 DME GAMES
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "US_GameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSHADOWS_LOTL_API AUS_GameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
